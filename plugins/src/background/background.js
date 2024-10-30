3d36b1a95cbb82199251ea4929ab59944a9b0184
lakshitrajput <lakshit1729@gmail.com>
2024-10-30 18:27:37 +0530

    Updated: popup window

--- plugins/src/background/background.js
+++ plugins/src/background/background.js
@@ -1,6 +1,42 @@
 // TODO: background script
 // chrome.runtime.onInstalled.addListener(() => {
+// }
+chrome.action.onClicked.addListener(function() {
+    chrome.system.display.getInfo(function(displays) {
+        let primaryDisplay = displays.find(display => display.isPrimary);
+
+        var w = 700;
+        var h = 520;
+        var left = (primaryDisplay.bounds.width / 2) - (w / 2);
+        var top = (primaryDisplay.bounds.height / 2) - (h / 2);
+
+        chrome.windows.create({
+            'url': 'popup.html',
+            'type': 'popup',  
+            'width': w,
+            'height': h,
+            'left': left,
+            'top': top
+        }, function(popupWindow) {
+            if (chrome.runtime.lastError) {
+                console.error(chrome.runtime.lastError);
+                return;
+            }
+
+            // event listener to close the window when clicking outside
+            chrome.windows.onFocusChanged.addListener(function(windowId) {
+                if (windowId !== popupWindow.id && windowId !== chrome.windows.WINDOW_ID_NONE) {
+                    chrome.windows.getAll({}, function(windows) {
+                        if (windows.some(win => win.id === popupWindow.id)) {
+                            chrome.windows.remove(popupWindow.id);
+                        }
+                    });
+                }
+            });
+        });
+    });
+});
 
 
-// })
+
 
