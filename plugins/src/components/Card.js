f8705819aa122f34afbd5e5cbb4d87823bf9ae64
harshit1142 <harshitpandey98765@gmail.com>
2024-10-31 19:15:12 +0530

    ui:fixes

--- plugins/src/components/Card.js
+++ plugins/src/components/Card.js
@@ -3,12 +3,11 @@ import React from 'react'
 const Card = ({ tab }) => {
   return (
 
-      <div className="card m-2" style={{ width: "10rem" }}>
-        {console.log(tab.favIconUrl === "")}
-          <img src={(tab.favIconUrl) === "" ? "https://www.scwitservices.com/images/easyblog_shared/February_2021/2-17-21/b2ap3_thumbnail_131006821_multiple_tabs_400.jpg" : tab.favIconUrl}
+      <div className="card m-2 overflow-hidden d-flex justify-content-center" style={{ width: "180px",height: "180px" }}>
+          <img style={{width: "100px", height: "100px", objectFit: "fit",alignItems: "center" ,position: "relative", top: "20px", left: "40px"}} src={(tab.favIconUrl) === "" ? "https://www.scwitservices.com/images/easyblog_shared/February_2021/2-17-21/b2ap3_thumbnail_131006821_multiple_tabs_400.jpg" : tab.favIconUrl}
               className="card-img-top" alt="..." />
           <div className="card-body text-center">
-              <h5 className="card-title">{tab.title}</h5>
+              <h5 className="card-title" style={{fontSize: "15px"}} >{tab.title}</h5>
           </div>
       </div>
   )
