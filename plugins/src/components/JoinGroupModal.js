91e460ccbfd9532fc6add2b08c370cc58e09a504
harshit1142 <harshitpandey98765@gmail.com>
2024-11-09 17:20:09 +0530

    Added: Group copy

--- plugins/src/components/JoinGroupModal.js
+++ plugins/src/components/JoinGroupModal.js
@@ -1,6 +1,37 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 export default function JoinGroupModal({ setShowJoinGroup }) {
+
+    const [groupID, setGroupID] = React.useState('');
+    const navigate=useNavigate();
+
+  const handleSubmit=async(e)=>{
+      e.preventDefault();
+      const authToken = localStorage.getItem('authToken');
+      const res = await fetch(`http://localhost:4000/api/group/add`, {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${authToken}`
+          },
+          body: JSON.stringify({
+              groupCode: groupID
+          }),
+      });
+      const response = await res.json();
+      console.log(response);
+      
+      if(response.success){
+          toast.success(response.msg);
+          navigate("/");
+          setShowJoinGroup(false);
+      }else{
+          toast.error(response.msg);
+      }
+  }
+
   return (
       <div className='home'>
           <div className='home-content'>
@@ -13,10 +44,10 @@ export default function JoinGroupModal({ setShowJoinGroup }) {
                   <form>
                       <div class="form-group mt-5">
                           <label for="exampleInputEmail1">Join Group</label>
-                          <input type="email" class="form-control mt-3" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter 6 character group ID" />
+                          <input type="email" class="form-control mt-3" id="exampleInputEmail1" value={groupID} onChange={(e) => setGroupID(e.target.value)} aria-describedby="emailHelp" placeholder="Enter 6 character group ID" />
 
                       </div>
-                      <button type="submit" class="btn btn-primary mt-3">Submit</button>
+                      <button type="submit" class="btn btn-primary mt-3" onClick={handleSubmit}>Submit</button>
                   </form>
               </div>
           </div>
